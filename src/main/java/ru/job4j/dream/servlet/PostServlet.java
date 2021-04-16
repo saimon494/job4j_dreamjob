bc83db1e476ff4bf442d261497335ba7c2bd27b1
saimon494 <saimon.494@gmail.com>
2021-04-16 23:49:51 +0300

    0. MVC в Servlet. [#500291]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -12,13 +12,20 @@ import java.io.IOException;
 public class PostServlet extends HttpServlet {
 
     @Override
-    protected void doPost(
+    protected void doGet(
             HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.getRequestDispatcher("posts.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(
+            HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().save(
                 new Post(
                         Integer.parseInt(req.getParameter("id")),
                         req.getParameter("name")));
-        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+        resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 }
